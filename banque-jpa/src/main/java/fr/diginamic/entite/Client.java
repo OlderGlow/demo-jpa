4a56a291bfb634de6bcfdaaa95c61354b2e5858d
Julien Picquet <julienpicquet1@gmail.com>
2022-06-28 14:45:59 +0200

    edit cascade fetch on client

--- banque-jpa/src/main/java/fr/diginamic/entite/Client.java
+++ banque-jpa/src/main/java/fr/diginamic/entite/Client.java
@@ -22,7 +22,7 @@ public class Client {
     @Embedded
     private Adresse adresse;
 
-    @ManyToOne
+    @ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
     private Banque banque;
 
     @ManyToMany
